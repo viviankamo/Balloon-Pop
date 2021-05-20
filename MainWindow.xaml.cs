37f4eedd25d049adbe0373bd70a19c18e352d1dc
viviankamo <vivian.kamo@elev.ga.ntig.se>
2021-05-20 10:58:26 +0200

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,13 +21,14 @@ namespace Balloon_Pop
     
     // Detta bestämmer hur snabbt och hur ofta ballongerna ska komma in i spelet //
     { 
+        // Variabler för spelet (tid, snabbhet och intervall)// 
         DispatcherTimer gameTimer = new DispatcherTimer();
 
         int speed = 3;
         int intervals = 90;
         Random rand = new Random();
 
-        // Tar bort balongenrna som har gått förbi toppen från canvasen //
+        // Ballongerna som har gått förbi toppen av canvasen läggs till i itemremover och det tar bort dom från canvasen //
         List<Rectangle> itemRemover = new List<Rectangle>();
 
         // Importerar bakrundsbild //
@@ -37,7 +38,7 @@ namespace Balloon_Pop
         int balloonSkins;
         int i;
 
-        // Gör så att man vet när spelet är aktivt och när det är över //
+        // Gör så att man vet när spelet är aktivt och när det är över, när man missar ett antal balonger då är spelet över //
         int missedBalloons;
 
         bool gameIsActive;
@@ -49,14 +50,14 @@ namespace Balloon_Pop
 
 
         public MainWindow()
-            // Detta ska hålla koll på alla ballonger //
+            // Ett event som ska hålla koll på alla ballonger //
         {
             InitializeComponent();
 
             gameTimer.Tick += GameEngine;
             gameTimer.Interval = TimeSpan.FromMilliseconds(20);
 
-            // Lokaliserar bakgrundsbilden //
+            // Lokaliserar var bakgrundbilden ligger i mappen files //
             backgroundImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/files/background-Image.jpg"));
             MyCanvas.Background = backgroundImage;
 
@@ -66,7 +67,8 @@ namespace Balloon_Pop
         private void GameEngine(object sender, EventArgs e)
         {
             // GameEngine ska hålla koll på alla balonger //
-            // Visar uppdaterad poängtavla //
+            // Visar uppdaterad poängtavla. //
+            // Om ballonoskin numret är 1,2,3,5 då kan man knyta en image(bild) till Imagebrush och sedan till rectangle //
             scoreText.Content = "Score: " + score;
 
             intervals -= 10;
@@ -77,12 +79,14 @@ namespace Balloon_Pop
 
                 balloonSkins += 1;
 
-            // Behövs inte högre nummer då det bara finns 5 olika ballonger //
+            // Behövs inte högre nummer då det bara finns 5 olika ballonger som existerar. //
                 if (balloonSkins > 5)
                 {
                     balloonSkins = 1;
                 }
 
+                // Detta lokaliserar ballongerna som ligger i files //
+                // switch sats påminner om if sats men används för att hantera olika cases(utfall) som här till exempel //
                 switch (balloonSkins)
                 {
                     case 1:
@@ -101,7 +105,8 @@ namespace Balloon_Pop
                         balloonImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/files/balloon5.png"));
                         break;
                 }
-
+                // Här definieras properties(egenskaper) för rectangle bland annat höjd och bredd. //
+                // Den kommer direkt att länka till balloonimage så det nummer som är knyten till det kommer ladda den bilden och knytas till den nya ballongen //
                 Rectangle newBalloon = new Rectangle
                 {
                     Tag = "balloon",
@@ -110,15 +115,17 @@ namespace Balloon_Pop
                     Fill = balloonImage
                 };
 
-                // x och y postionen av ballongen //
+                // x och y postionen av ballongen. Left är ett random nummer medans resten har ett specifikt nummer. //
                 Canvas.SetLeft(newBalloon, rand.Next(50, 400));
                 Canvas.SetTop(newBalloon, 600);
 
                 MyCanvas.Children.Add(newBalloon);
 
+                // Varje gång vi skapar en ballong (under 1) följer den alla instruktioner och skapar en ny ballong får det ett nytt nummer //
                 intervals = rand.Next(90, 150);
             }
 
+            // Här letas det efter rectangle med (tag = balloon) alla rectangles med denna taggen kan man flytta upp och åt sidan. //
             foreach (var x in MyCanvas.Children.OfType<Rectangle>())
             {
                 if ((string)x.Tag == "balloon")
@@ -137,13 +144,13 @@ namespace Balloon_Pop
                 }
 
             }
-            // ballongerna som når toppen försvinner //
+            // ballongerna som når toppen och ligger i itemremover försvinner //
             foreach (Rectangle y in itemRemover)
             {
                 MyCanvas.Children.Remove(y);
             }
 
-            // Missar man 10 balonger förlorar man och får ett meddelande för att kunna starta om spelet igen //
+            // Missar man 10 balonger förlorar man och spelet är inte aktivt då får man ett meddelande för att kunna starta om spelet igen //
             if (missedBalloons > 10)
             {
                 gameIsActive = false;
@@ -164,6 +171,7 @@ namespace Balloon_Pop
         private void popBalloons(object sender, MouseButtonEventArgs e)
         {
             //Man ska kunna klicka ballongerna så de spricks när spelet är aktivt och höra ljudet när de spricker //
+            // När detta händer får man ett poäng //
             if (gameIsActive)
             {
                 if (e.OriginalSource is Rectangle)
@@ -183,7 +191,7 @@ namespace Balloon_Pop
 
         private void StartGame()
         {
-            // Hur det ska vara och se ut när spelet startar och är aktivt // 
+            // När spelet startar ska det vara inga missade ballonger, 0 poäng, specifikt tempo och då är spelet aktivt // 
             gameTimer.Start();
 
             missedBalloons = 0;
@@ -196,13 +204,13 @@ namespace Balloon_Pop
 
         private void RestartGame()
         { 
-            // om det existerar rectangles ska de läggas till i itemremover //
+            // om det existerar rectangles ska de läggas till i itemremover, det är två foreach loopar som används till detta//
             foreach (var x in MyCanvas.Children.OfType<Rectangle>())
             {
                 itemRemover.Add(x);
             }
 
-            // Alla rectangles som är i itemremover kan tas bort från canvasen //
+            // Alla rectangles som är i itemremover kommer tas bort från canvasen, då resetar det till 0 och man kan köra igen //
             foreach (Rectangle y in itemRemover)
             {
                 MyCanvas.Children.Remove(y);
